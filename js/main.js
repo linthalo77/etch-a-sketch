ddace8bac1fd2c2dd2829a453468ee34e868269e
linthalo <vincentahluwalia@protonmail.com>
2025-03-09 14:07:43 +0100

    Refactor code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,30 @@
 const gridContainer = document.querySelector(".grid-container");
-const btnSquares = document.querySelector(".square-num-button");
+const changeGrid = document.querySelector(".change-grid");
+
 const sketchboardSize = 800;                                                // 800x800 px
-let rowSize;
-let squaresToBeGenerated;
-let squareSize;
 
-window.onload = () => {
-    console.log("Started Code");
-    rowSize = 16;                                                           // default 16 x 16
-    squaresToBeGenerated = rowSize * rowSize + (rowSize + 1);               // need to add (squareNum + 1) to compensate for line break squares
-    squareSize = (sketchboardSize / rowSize) -2;                            // -2 because of border size
-    generateGrid();
-    addHover();
-}
+function generateGrid(size) {
+    let squareSize = sketchboardSize / size - 2;
+    for (let i = 0; i < (+size * +size); ++i) {
+        const square = document.createElement("div");
 
-function generateGrid() {
-    for (let i = 1; i < squaresToBeGenerated; ++i) {
-        const div = document.createElement("div");
-        if (!(i % (rowSize + 1))) {
-            div.setAttribute("style", "width: 100%; height: 0px; border: 0px;");
-        } else {
-            div.classList.add("square");
-            div.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px; border: 1px solid black`);
-        }
-        gridContainer.appendChild(div);
+        square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px; border: 1px solid black`);
+        square.classList.toggle("square");
+
+        gridContainer.appendChild(square);
     }
+    
+    addHover();
 }
 
 function addHover() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.addEventListener("mouseenter", function (e) {
-            e.target.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+            const red = Math.round(Math.random() * 255);
+            const green = Math.round(Math.random() * 255);
+            const blue = Math.round(Math.random() * 255);
+            e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
         })
     });
 }
@@ -43,21 +36,18 @@ function deleteGrid() {
     });
 }
 
-btnSquares.addEventListener("click", () => {
-    while(true) {
-        rowSize = prompt("Size of grid? (eg. 16 = 16x16 grid)");
-        if (rowSize > 100) {
-            alert("Sorry! Values over 100 are not allowed.")
-        } else {
-            break;
-        }
+
+generateGrid(16);
+
+changeGrid.addEventListener("click", () => {
+    let size = +prompt("Size of grid? (eg. 16 = 16x16 grid)");
+    while(size > 100 || size < 1) {
+        alert("Sorry! Values over 100 are not allowed.")
+        size = +prompt("Size of grid? (eg. 16 = 16x16 grid)");
     }
-    rowSize = +rowSize;
-    squaresToBeGenerated = rowSize * rowSize + (rowSize + 1);               // need to add (squareNum + 1) to compensate for line break squares
-    squareSize = (sketchboardSize / rowSize) -2;                            // -2 because of border size
+
     deleteGrid();
-    generateGrid();
-    addHover();
+    generateGrid(size);
 })
 
 
