063d2555d2348d24ff6bfba2a61e3ac4d40d8152
linthalo <vincentahluwalia@protonmail.com>
2025-03-09 12:20:00 +0100

    Implement variable grid size

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 const gridContainer = document.querySelector(".grid-container");
+const btnSquares = document.querySelector(".square-num-button");
 
-for (let i = 1; i < 273; ++i) {
+let sketchboardSize = 800; // 800x800 px
+
+let squareNum = 21; // default 16 x 16
+btnSquares.addEventListener("click", () => {
+    squareNum = prompt("Size of grid? (eg. 16 = 16x16 grid)");
+    console.log(squareNum);
+})
+
+let squaresToBeGenerated = squareNum * squareNum + (squareNum + 1)
+let squareSize = Math.round((sketchboardSize / squareNum) - 2);       // -2 because of border size
+console.log(squareSize);
+
+// divide height and width / num of squares to get height and width for squares
+for (let i = 1; i < squaresToBeGenerated; ++i) {
     const div = document.createElement("div");
-    if (!(i % 17)) {
+    if (!(i % (squareNum + 1))) {
         div.setAttribute("style", "width: 100%; height: 0px; border: 0px;");
     } else {
-        div.setAttribute("style", "height: 25px; width:25px; border: 1px solid black");
+        div.classList.toggle("square");
+        div.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px; border: 1px solid black`);
     }
     gridContainer.appendChild(div);
 }
 
+
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+    let temp = square.getAttribute("background-color")
+    square.addEventListener("mouseenter", function (e) {
+        e.target.style.backgroundColor = "green";
+    })
+    square.addEventListener("mouseleave", function (e) {
+        e.target.style.backgroundColor = "green";
+    })
+});
 
 
